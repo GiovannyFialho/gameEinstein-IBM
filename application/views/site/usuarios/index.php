f85d34dcd514d208fa577a63eda1bebee835d76b
GiovannyFialho <giovannyf@outlook.com>
2021-08-14 23:11:52 -0300

    criacao de pagina de cadastro

--- application/views/site/usuarios/index.php
+++ application/views/site/usuarios/index.php
@@ -1,15 +1,35 @@
-<div class="form-container">
-    <h2 class="titulo-componente">Cadastrar</h2>
+<div class="container-cadastrar">
+    <div class="container">
+        <div class="form-container">
+            <form class="form" id="form-cadastro">
+                <div class="form-main">
+                    <div class="input-item">
+                        <input type="text" id="name" placeholder="Nome" require>
+                    </div>
+                    <div class="input-item">
+                        <input type="email" id="email" placeholder="E-mail" require>
+                    </div>
+                    <div class="input-item">
+                        <input type="password" id="password" placeholder="Senha" require>
+                    </div>
+                </div>
 
-    <form class="form-default" id="form-cadastro">
-        <input type="text" id="name" placeholder="Nome" require>
-        <input type="email" id="email" placeholder="E-mail" require>
-        <input type="password" id="password" placeholder="Senha" require>
-
-        <div class="button-container">
-            <button>Cadastrar</button>
+                <div class="form-footer">
+                    <a href="#" class="link-padrao">
+                        Confira regulamento
+                    </a>
+                    <div class="button-container">
+                        <button>
+                            Cadastrar
+                            <span class="icon">
+                                <svg focusable="false" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" fill="currentColor" aria-label="Open resource" width="20" height="20" viewBox="0 0 32 32" role="img"><path d="M26,28H6a2.0027,2.0027,0,0,1-2-2V6A2.0027,2.0027,0,0,1,6,4H16V6H6V26H26V16h2V26A2.0027,2.0027,0,0,1,26,28Z"></path><path d="M20 2L20 4 26.586 4 18 12.586 19.414 14 28 5.414 28 12 30 12 30 2 20 2z"></path></svg>
+                            </span>
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
-    </form>
+    </div>
 </div>
 
 <div class="container-popup">
