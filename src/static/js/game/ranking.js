f010c673cb81a33045435b3a7073818f18c34d43
GiovannyFialho <giovannyf@outlook.com>
2021-08-16 19:16:17 -0300

    alinhando tabela de rankings

--- src/static/js/game/ranking.js
+++ src/static/js/game/ranking.js
@@ -2,14 +2,18 @@ let ranking = document.querySelector(".ranking-container");
 
 if (ranking) {
     const createTable = async () => {
-        const response = await fetch(`${location.origin}/game/getRanking`);
+        const response = await fetch(`${location.origin}/game/getRanking`, {
+            cache: "no-cache",
+        });
         const items = await response.json();
+
+        console.log(items);
 
         items.data.forEach((item, index) => {
             document.getElementById(`ranking`).innerHTML += `
                 <div class="ranking-container-item">
-                    <p>${index}</p>
-                    <p>${item.idUser}</p>
+                    <p>${index + 1}</p>
+                    <p>${item.name}</p>
                 </div>
             `;
         });
