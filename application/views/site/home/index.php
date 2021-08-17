284e318172d60716478e488ee51b502290a9d99a
GiovannyFialho <giovannyf@outlook.com>
2021-08-16 22:42:56 -0300

    ajustes mobile

--- application/views/site/home/index.php
+++ application/views/site/home/index.php
@@ -5,12 +5,21 @@
             <p>Por isso, desafiamos você a viver uma experiência virtual baseada na lógica, no raciocínio rápido e em alguns assuntos abordados do evento.</p>
             <p>Os três primeiros colocados serão contemplados com experiências reais no mundo off-line, vivenciando uma conquista inovadora, transformadora e híbrida.</p>
             <div class="button-container">
-                <a href="/usuarios">
-                    Cadastrar
-                    <span class="icon">
-                        <svg focusable="false" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" fill="currentColor" aria-label="Open resource" width="20" height="20" viewBox="0 0 32 32" role="img"><path d="M26,28H6a2.0027,2.0027,0,0,1-2-2V6A2.0027,2.0027,0,0,1,6,4H16V6H6V26H26V16h2V26A2.0027,2.0027,0,0,1,26,28Z"></path><path d="M20 2L20 4 26.586 4 18 12.586 19.414 14 28 5.414 28 12 30 12 30 2 20 2z"></path></svg>
-                    </span>
-                </a>
+                <?if(!$this->session->userSession){?>
+                    <a href="/usuarios">
+                        Cadastrar
+                        <span class="icon">
+                            <svg focusable="false" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" fill="currentColor" aria-label="Open resource" width="20" height="20" viewBox="0 0 32 32" role="img"><path d="M26,28H6a2.0027,2.0027,0,0,1-2-2V6A2.0027,2.0027,0,0,1,6,4H16V6H6V26H26V16h2V26A2.0027,2.0027,0,0,1,26,28Z"></path><path d="M20 2L20 4 26.586 4 18 12.586 19.414 14 28 5.414 28 12 30 12 30 2 20 2z"></path></svg>
+                        </span>
+                    </a>
+                <?}else{?>
+                    <a href="/game">
+                        Desafio
+                        <span class="icon">
+                            <svg focusable="false" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" fill="currentColor" aria-label="Open resource" width="20" height="20" viewBox="0 0 32 32" role="img"><path d="M26,28H6a2.0027,2.0027,0,0,1-2-2V6A2.0027,2.0027,0,0,1,6,4H16V6H6V26H26V16h2V26A2.0027,2.0027,0,0,1,26,28Z"></path><path d="M20 2L20 4 26.586 4 18 12.586 19.414 14 28 5.414 28 12 30 12 30 2 20 2z"></path></svg>
+                        </span>
+                    </a>
+                <?}?>
             </div>
         </div>
     </div>
