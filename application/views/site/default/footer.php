951b1ae54655ecd52c1eb16cb74c6ae780d72f94
GiovannyFialho <giovannyf@outlook.com>
2021-08-19 22:22:08 -0300

    adicionando footer nas paginas

--- application/views/site/default/footer.php
+++ application/views/site/default/footer.php
@@ -1,5 +1,30 @@
         </main>
-        <footer></footer>
+        <footer>
+            <div class="container">
+                <div class="head">
+                    <a href="#" class="head-link">
+                        Contato
+                    </a>
+                    <a href="#" class="head-link">
+                        Privacidade
+                    </a>
+                    <a href="#" class="head-link">
+                        Termos de Uso
+                    </a>
+                    <a href="#" class="head-link">
+                        Acessibilidade
+                    </a>
+                </div>
+                <div class="body">
+                    <p>
+                        &#169 IBM Corporation 2020. IBM, o logotipo da IBM, IBM Cloud™️ e ibm.com são marcas da International Business Machines Corp. registadas em muitas jurisdições no mundo. Este documento está atualizado na data da sua publicação e pode ser alterado pela IBM a qualquer momento. Se não deseja mais receber e-mails desta campanha, clique aqui.. Saiba mais sobre como a IBM trata os seus dados pessoais consultando a nossa política de privacidade. Em relação as informações deste e-mail e campanha, elas são fornecidas pela MCM Brand Experience, e para saber como trata os seus dados pessoais, consulte a política de privacidade ou entre em contato pelo e-mail contato@mcm.br.com para quaisquer solicitações referente a seus dados.
+                    </p>
+                </div>
+                <div class="footer">
+                    <p>&#169 Todos os direitos resevados - Desenvolvimento por MCM Brand Experience</p>
+                </div>
+            </div>
+        </footer>
         <script src="<?= base_url("assets") ?>/build/js/script.min.js"></script>
     </body>
 </html>
